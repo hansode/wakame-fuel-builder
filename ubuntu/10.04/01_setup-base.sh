c0c0424c6fc06bcc3a5dfc6f9bfb3aa53b85c9e2
Masahito Yoshida <masahito@axsh.net>
2010-08-02 12:32:49 +0900

    first commit

---
+++ ubuntu/10.04/01_setup-base.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+# Ubuntu-10.04 LSD
+#
+
+export LANG=C
+export DEBIAN_FRONTEND=noninteractive
+
+# core packages
+pkgs="
+ ruby ruby1.8-dev rdoc1.8 ri1.8 rubygems1.8
+ rabbitmq-server
+ make g++ libopenssl-ruby libsqlite3-dev
+ ec2-ami-tools
+"
+
+[ -f /etc/apt/sources.list ] && {
+  perl -pi -e 's, multiverse,,g; s,$, multiverse,' /etc/apt/sources.list
+}
+apt-get update
+apt-get -y install ${pkgs}
+
+# wakame account
+getent group  wakame >/dev/null || {
+  groupadd wakame
+}
+getent passwd wakame >/dev/null || {
+  useradd -d /home/wakame -s /bin/bash -g wakame -m wakame
+}
+
+# wakame installation
+echo "#Setting up wakame ..."
+su - wakame -c "
+  gem list | grep -q -w wakame || {
+    #gem install /tmp/wakame-0.5.1.gem --no-rdoc --no-ri
+    gem install wakame --no-rdoc --no-ri
+  }
+"
+[ -d /home/wakame/wakame.proj ] || {
+  su - wakame -c "
+    /home/wakame/.gem/ruby/1.8/bin/wakame /home/wakame/wakame.proj
+  "
+}
+
+# wakame system configuration
+cat <<EOS > /etc/default/wakame
+GEM_HOME=/home/wakame/.gem/ruby/1.8/
+WAKAME_ROOT=/home/wakame/wakame.proj/
+WAKAME_ENV=EC2
+WAKAME_CLUSTER_ENV=development
+EOS
+
+# add service
+[ -L /etc/init.d/wakame-master ] || {
+  ln -s /home/wakame/wakame.proj/config/init.d/wakame-master /etc/init.d/
+  /usr/sbin/update-rc.d wakame-master defaults 40
+}
+[ -L /etc/init.d/wakame-agent ] || {
+  ln -s /home/wakame/wakame.proj/config/init.d/wakame-agent /etc/init.d/
+  /usr/sbin/update-rc.d wakame-agent  defaults 41
+}
+
+# ssh key pair
+[ -d /home/wakame/config ] || {
+  su - wakame -c "
+    mkdir /home/wakame/config
+  "
+}
+[ -f /home/wakame/config/root.id_rsa ] || {
+  su - wakame -c "
+    ssh-keygen \
+     -t rsa \
+     -N '' \
+     -C wakame-master \
+     -f /home/wakame/config/root.id_rsa
+  "
+  cat /home/wakame/config/root.id_rsa.pub >> /root/.ssh/authorized_keys
+}
+
+# disable apparmor
+[ -x /etc/init.d/apparmor ] && {
+  /etc/init.d/apparmor stop
+  /usr/sbin/update-rc.d -f apparmor remove
+}
---
+++ ubuntu/10.04/02_build-demo.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+#
+# Ubuntu-10.04 LSD
+#
+# ELB -> nginx -> spawn-fcgi -> fcgiwrap
+
+export LANG=C
+export DEBIAN_FRONTEND=noninteractive
+
+# core packages
+pkgs="
+ nginx
+ spawn-fcgi libfcgi-dev
+ automake
+ subversion git-core
+"
+
+[ -f /etc/apt/sources.list ] && {
+  perl -pi -e 's, multiverse,,g; s,$, multiverse,' /etc/apt/sources.list
+}
+apt-get -y install ec2-ami-tools
+
+apt-get -y install ${pkgs}
+
+[ -x /etc/init.d/nginx ] && {
+  /etc/init.d/nginx stop
+  /usr/sbin/update-rc.d -f nginx remove
+}
+
+su - wakame -c "
+  /home/wakame/wakame.proj/script/generate resource nginx
+  /home/wakame/wakame.proj/script/generate resource ec2_elb
+"
+
+
+# fcgiwrap
+cd /tmp
+[ -f gnosek-fcgiwrap-latest.tar.gz ] && rm -f gnosek-fcgiwrap-latest.tar.gz
+
+wget http://github.com/gnosek/fcgiwrap/tarball/master -O gnosek-fcgiwrap-latest.tar.gz || exit 1
+dirname=$(tar ztf  gnosek-fcgiwrap-latest.tar.gz | head -1)
+
+[ -d $dirname ] && rm -rf $dirname
+
+tar zxvf gnosek-fcgiwrap-latest.tar.gz || exit 1
+
+cd $(tar ztf  gnosek-fcgiwrap-latest.tar.gz | head -1) || exit 1
+autoheader  || exit 1
+autoconf    || exit 1
+./configure || exit 1
+make clean  || exit 1
+make        || exit 1
+
+install -m 755 fcgiwrap /usr/local/bin/fcgiwrap
+
+
+cat <<'__EOS__' > /var/www/index.cgi
+#!/bin/sh
+
+api_base_uri=http://169.254.169.254/latest/meta-data/
+
+retrieve_meta_data() {
+  curl -s -f --retry 3 ${api_base_uri}$1
+}
+
+meta_data() {
+  local param=$1
+
+  echo ${param} | egrep '/$' -q && {
+    for i in $(retrieve_meta_data ${param}); do
+      meta_data ${param}${i}
+    done
+  } || {
+    echo -n "${param} = "
+    echo $(retrieve_meta_data ${param})
+  }
+}
+
+cat <<EOS
+Content-type: text/plain
+
+EOS
+
+echo "[wakame demo info]-----"
+date +%Y/%m/%d-%H:%M:%S
+hostname
+echo
+
+echo "[meta-data]-----"
+meta_data /
+echo
+
+echo "[env]-----"
+env
+
+exit 0
+__EOS__
+
+chmod +x /var/www/index.cgi
